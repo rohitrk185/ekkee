dabec6a7a178b21307f064c2289fef9ad9457832
rkMylang <rohitsmudge190@gmail.com>
2025-07-13 12:29:57 +0530

    Add onboarding page and update dependencies
    Introduced a new onboarding page component. Updated Tailwind CSS, PostCSS, and related dependencies to latest versions. Simplified the main page content and refactored PostCSS config for compatibility.

---
+++ frontend/src/app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Implement SSG and pass it to the component below
+type Props = {};
+
+const page = (props: Props) => {
+  return <div className="text-xl text-blue-500">page</div>;
+};
+
+export default page;
